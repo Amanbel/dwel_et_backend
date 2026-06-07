05a2f5eafeda501aaed3dd806136a45ee0d949b7
Amanbel <amanuel11belachew@gmail.com>
2026-06-07 23:23:38 +0300

    changes: users/me response changed

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -4,5 +4,9 @@ import { findUserById, publicUser } from "../repositories/user.repository";
 export const me = async (req: Request, res: Response) => {
   const user = await findUserById((req as any).user.userId);
   if (!user) return res.status(404).json({ message: "User not found" });
-  res.json(publicUser(user));
+  res.json({
+    status: "connected user",
+    ok: true,
+    user: publicUser(user),
+  });
 };
